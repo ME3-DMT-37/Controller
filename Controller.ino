2d14a3e4fda5baab4a4337e3ba37b796c02be9a6
rc2513 <rc2513@ic.ac.uk>
2016-05-17 14:22:45 +0100

    Merged with existing code
    Combined with code for individual strings written by Chris.

--- Controller.ino
+++ Controller.ino
@@ -1,5 +1,37 @@
-int motor_pin[] = {3, 4, 5, 6, 9, 10};
+#include <Audio.h>
+#include <SD.h>
+#include <SerialFlash.h>
+#include <SPI.h>
+#include <Wire.h>
+
+// ----------------------------------------------------------------
+
+AudioInputAnalog          adc;
+AudioAnalyzeNoteFrequency note;
+AudioAnalyzePeak          peak;
+AudioConnection           patchCord(adc, note);
+AudioConnection           patchCord2(adc, peak);
+
+// ----------------------------------------------------------------
+
+int motor_pin[] = {3, 4, 5, 6, 10, 9};
 int direction_pin = 11;
+
+// ----------------------------------------------------------------
+
+float string_min[] = {81.94, 109.37, 145.98, 194.87, 245.52, 327.73};
+float string_max[] = {82.89, 110.64, 147.68, 197.14, 248.37, 331.54};
+
+float speed_forward[] = {65, 50, 80, 90, 50, 90};
+float speed_reverse[] = {30, 40, 40, 50, 30, 70};
+
+// ----------------------------------------------------------------
+
+int string = 0;
+int waited = 0;
+int tuned = 0;
+
+// ----------------------------------------------------------------
 
 void setup() {
 
@@ -12,17 +44,76 @@ void setup() {
   pinMode(motor_pin[5], OUTPUT);
   pinMode(direction_pin, OUTPUT);
 
+  Serial.begin(9600);
+
+  AudioMemory(30);
+  note.begin(0.15);
+
+  delay(1000);
+
+  Serial.printf("tuning string %d to between %3.2f and %3.2f Hz\n", string + 1, string_min[string], string_max[string]);
+
 }
+
+// ----------------------------------------------------------------
 
 void loop() {
 
-  // run each motor for one second
-  for (int i = 0; i < 6; i++) {
-    motorRun(i, 100);
-    delay(1000);
+  // check if a note is available
+  if (note.available()) {
+
+    // read frequency
+    float f = note.read();
+
+    // read peak voltage
+    float p = peak.read() * 1.2;
+
+    // Serial.printf("Note: %3.2f Hz\n", f);
+    // Serial.printf("Peak: %3.2f V\n", p);
+
+    if (p > 0.5) {
+
+      Serial.printf("%3.2f\n", f);
+
+      if (!tuned) {
+
+        if (f > string_max[string] && waited == 0) {
+          motorRun(string, speed_reverse[string]*(-1));
+        }
+        else if (f < string_min[string] && waited == 0) {
+          motorRun(string, speed_forward[string]);
+        }
+        else if (waited == 0) {
+          motorRun(string, 0);
+          delay(500);
+          Serial.println("waiting");
+          waited = 1;
+        }
+        else if (waited == 1 && f < string_max[string] && f > string_min[string]) {
+          Serial.println("tuned");
+          tuned = 1;
+        }
+        else {
+          Serial.println("try again");
+          waited = 0;
+        }
+
+      }
+
+    } else {
+      // too quiet so stop
+      motorRun(string, 0);
+    }
+  } else {
+    // no note available so stop
+    motorRun(string, 0);
   }
 
+  delay(100);
+
 }
+
+// ----------------------------------------------------------------
 
 void motorRun(int motor, int speed) {
 
@@ -42,7 +133,7 @@ void motorRun(int motor, int speed) {
 
   // set all motors off
   for (int i = 0; i < 6; i++) {
-    digitalWrite(motor_pin[i], direction);
+    analogWrite(motor_pin[i], direction * 255);
   }
 
   // set only requested motor on
